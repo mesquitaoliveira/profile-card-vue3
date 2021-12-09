6e8509abfdc4daff7905e61c85656c6c2777d2b0
PoroutroAngulo <92008139+mesquitaoliveira@users.noreply.github.com>
2021-12-09 05:02:34 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+
+<h1 align=center>Profile Card - <a target="_blank" href="https://profile-card-jade.vercel.app/" rel="nofollow">Demo</a> </h1>
+
+
+<p align=center>
+  <img src="https://img.shields.io/badge/Vue.js-35495E?style=for-the-badge&logo=vuedotjs&logoColor=4FC08D
+" alt="Vuejs">
+  <img src="https://img.shields.io/badge/replit-667881?style=for-the-badge&logo=replit&logoColor=white
+" alt="replit">
+  <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" alt="Html">
+ <img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" alt="Css">
+ <img alt="GitHub code size in bytes" src="https://img.shields.io/github/languages/code-size/mesquitaoliveira/profile-card?style=for-the-badge">
+ <img alt="GitHub forks" src="https://img.shields.io/github/forks/mesquitaoliveira/profile-card?style=for-the-badge">
+ 
+ <img alt="GitHub" src="https://img.shields.io/github/license/mesquitaoliveira/profile-card?style=for-the-badge">
+
+</p>
+
+---
+<p align="center">
+  <img src="./assets/demo.gif"
+    alt="screenshot" width="100%">
+</p>
+
+---
+
+## Build Setup
+```bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run all tests
+npm test
+
+```
+---
+## Deployment and hosting
+
+
+[![Deploy toNetlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/mesquitaoliveira/profile-card)
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fmesquitaoliveira%2Fprofile-card&demo-title=Profile%20Card&demo-image=https%3A%2F%2Fgithub.com%2Fmesquitaoliveira%2Fprofile-card%2Fblob%2Fmaster%2Fimages%2Fdemo.gif)
+
+Follow the steps.
+## Special Thanks
+- [Google Fonts](http://fonts.google.com/)
+- [Font Awesome](https://fontawesome.com/)
---
+++ src/App.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="container">
+        <div class="cover-photo">
+            <img src="./assets/profile.png" class="profile">
+        </div>
+        <div class="profile-name">Jonh Doe</div>
+        <p class="about">
+            <i class="fab fab fa-ethereum"></i>
+            Ethereum developer and <br>
+            <i class="fab fab fas fa-dice-d6"></i> Web 3.0  
+        developer  </p>
+        <button class="msg-btn">Messege</button>
+        <button class="follow-btn">Following</button>
+        <div>
+            <i class="fab fab fa-hive"></i>
+            <i class="fab fa-linkedin"></i>
+            <i class="fab fa-youtube"></i>
+            <i class="fab fa-github"></i>        
+        </div>
+    </div>
+</template>
+
+<script> 
+  export default {
+  name: 'App'
+} 
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,300;0,400;0 de,500;1,500&display=swap');
+
+body{
+    font-family: 'Montserrat', sans-serif;
+    background-color: #015863;
+}
+.container{
+    user-select: none;
+    margin: 100px auto;
+    background: #161926;
+    color:#b3b8cd;
+    border-radius: 5px;
+    width: 350px;
+    text-align: center;
+}
+.cover-photo{
+    background: url(./assets/background.jpeg);
+    height: 160px;
+    border-radius: 5px 5px 0 0;
+}
+.container img{
+    position:center;
+    height: auto;
+    width:auto;
+    max-width: 120px; 
+    max-height: 120px;
+    overflow:hidden;
+}
+.profile{
+    height: 120px;
+    width: 120px;
+    border-radius: 50%;
+    margin: 93px 0 0 -175px;
+    border: 1px solid #03bfbc;
+    padding: 5px;
+    background: #0e1430;
+}
+.profile-name{
+    font-size:25px;
+    font-weight:bold;
+    margin:27px 0 0 120px;
+}
+.about{
+    margin-top: 35px;
+    line-height: 21px;
+}
+button{
+    margin: 10px 0 40px 0;
+}
+.msg-btn, .follow-btn{
+    background:#03bfbc;
+    border:1px solid #03bfbc;
+    padding: 10px 25px;
+    color: #231e39;
+    border-radius:3px;
+    font-family: Montserrat, sans-serif;
+    cursor:pointer;
+}
+.msg-btn{
+    margin-left: 10px;
+    background:transparent;
+    color:#02899c;
+}
+.msg-btn:hover{
+    color:#231e39;
+    background:#03bfbc;
+    transition: 0.5s;
+}
+.follow-btn{
+    margin-left: 10px;
+    background:transparent;
+    color:#02899c;
+}
+.follow-btn:hover{
+    color:#231e39;
+    background:#03bfbc;
+    transition: 0.5s;
+}
+.container i{
+    padding-left: 20px;
+    font-size: 20px;
+    margin-bottom: 20px;
+    cursor:pointer;
+    transition: .5s;
+}
+.container i:hover{
+    color:#03bfbc;
+
+}
+</style>
